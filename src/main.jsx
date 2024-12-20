0622bce1fbd0678323709b8859ef65f0ff06d7fc
Esraa187 <mohammedesraa095@gmail.com>
2024-12-20 23:44:15 +0200

    finaaal

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'; 4
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import WeatherProvider from './context/WeatherContext';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <WeatherProvider>
         <App />
       </WeatherProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
